b0d376a3f466362d7793e88637f33ba50d6e3028
brs191 <rajashekar.b@gmail.com>
2018-06-06 15:35:25 +0900

    Add files via upload
    updated deleteTrieNode

--- Trie/Trie/main.c
+++ Trie/Trie/main.c
@@ -9,6 +9,10 @@
 
 #define ALPHABETS 26
 
+#define FREE(p) \
+    free(p); \
+    p = NULL;
+
 typedef struct _trieNode {
     struct _trieNode *children[ALPHABETS];
     int isEndOfWord;
@@ -32,9 +36,9 @@ void insertTrie(trieNode *root, const char *key) {
     trieNode *pCrawl = root;
     int len = strlen(key);
     int idx = 0;
-    for (int lvl = 0; lvl < len; lvl++) {
-        idx = CHAR_TO_INDEX(key[lvl]);
-        if(!pCrawl->children[idx]) {
+    for (int ch = 0; ch < len; ch++) {
+        idx = CHAR_TO_INDEX(key[ch]);
+        if(pCrawl->children[idx] == NULL) {
             pCrawl->children[idx] = newNode();
         }
         pCrawl = pCrawl->children[idx];
@@ -47,9 +51,9 @@ bool searchTrie(trieNode *root, const char *key) {
     trieNode *pCrawl = root;
     int idx = 0;
 
-    for (int lvl = 0; lvl < len; lvl++) {
-        idx = CHAR_TO_INDEX(key[lvl]);
-        if (!pCrawl->children[idx]) {
+    for (int ch = 0; ch < len; ch++) {
+        idx = CHAR_TO_INDEX(key[ch]);
+        if (pCrawl->children[idx] == NULL) {
             return false;
         }
         pCrawl = pCrawl->children[idx];
@@ -66,30 +70,63 @@ bool haveChildren(trieNode *node) {
     return false;
 }
 
+bool deleteHelper(trieNode *node, char *key, int lvl, int len) {
+    if (node) {
+        if (lvl == len) {
+            if (node->isEndOfWord) {
+                node->isEndOfWord = 0;
+                if (haveChildren(node) == false) {
+                    return true;
+                }
+                return false;
+            }
+        } else { // go recursive
+            int idx = CHAR_TO_INDEX(key[lvl]);
+            if(deleteHelper(node->children[idx], key, lvl+1, len)) {
+                printf("deleting char %c \n", idx + 'a');
+                FREE(node->children[idx]);
+                return ((node->isEndOfWord == false) && (haveChildren(node) == false));
+            }
+        }
+    }
+    return false;
+}
+
 //http://www.techiedelight.com/trie-implementation-insert-search-delete/
-void deleteTrie(trieNode *curr, char *str) {
-
-    if (*curr == NULL) {
-        return 0;
+void deleteTrie(trieNode *root, char *key) {
+    int len = strlen(key);
+    bool resul = false;
+    if (len > 0) {
+        resul = deleteHelper(root, key, 0, len);
     }
-
-    if (*str) {
-        if(*curr != NULL && (*curr)->)
-    }
+    printf("deleteTrie %s resul: %d\n", key, resul);
 }
 
 int main()
 {
-    char keys[][8] = {"the", "a", "there", "answer", "any", "by", "bye", "their"};
-    //char output[][32] = {"Not present in trie", "Present in trie"};
+    char keys[][8] = {"the", "a", "there", "answer", "any", "by", "bye", "their", "aws"};
+    char output[][32] = {"Not present in trie", "Present in trie"};
 
     root = newNode();
     for (int i = 0; i < ARRAY_SIZE(keys); i++) {
         insertTrie(root, keys[i]);
     }
     bool findRes = false;
-    findRes = searchTrie(root, "answer
-                         ");
-    printf ("seacrch: %d", findRes);
+    findRes = searchTrie(root, "answer");
+    printf ("seacrch: answer %s\n", output[findRes]);
+
+    findRes = searchTrie(root, "aws");
+    printf ("seacrch: aws %s\n", output[findRes]);
+
+    findRes = searchTrie(root, "any");
+    printf ("seacrch: any %s\n", output[findRes]);
+
+    deleteTrie(root, "aws");
+    deleteTrie(root, "any");
+    deleteTrie(root, "answer");
+    deleteTrie(root, "a");
+
+    findRes = searchTrie(root, "aws");
+    printf ("seacrch: aws %s\n", output[findRes]);
     return 0;
 }
