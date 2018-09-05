0e405c9415eacedf6745c50852cfe1bdcc2632a2
dhl <hmldh@naver.com>
2018-09-05 20:37:42 +0900

    second problem

---
+++ dhl/02_move_zeros/02_move_zeros.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define MAX 101
+char arr[MAX] = {0,};
+
+void swap(int a, int b) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+
+int main() {
+    int T, t;
+	freopen("input.txt", "r", stdin);
+	scanf("%d", &T);
+	for (t = 1; t <= T; t++) {
+        scanf("%s", arr);
+        int zero = 0, i;
+        for (i = 0; arr[i] != '\0'; i++) {
+            if (arr[i] == '0')
+                zero++;
+            else
+                swap(i, i - zero);
+        }
+        printf("%s\n", arr);
+    }
+	return 0;
+}
