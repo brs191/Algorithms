62ce8335a36e6be34c57dd92b8486395b5d6e39f
brs191 <rajashekar.b@gmail.com>
2018-09-05 13:50:55 +0900

    Update main.c

--- plusOne/main.c
+++ plusOne/main.c
@@ -18,21 +18,25 @@ void add() {
 
   int len = 0;
   int slen = 0;
+
   
-  if (len1 > len2) { slen = len1; len = len2; }
-  else { slen = len2; len = len1; }
-  
-  slen++;
+  if (len1 > len2) { slen = len1;}
+  else { slen = len2; }
+
+  len = slen;
+  slen++; // increase for extra
   int j = slen;
   sum[j] = '\0'; j--;
   for(int i = len-1; i >= 0; i--) {
-    if (len1 >= 0 && len2 >= 0) {
-      sum[j] = DIGIT(num1[len1-1]) + DIGIT(num2[len2-1]) + extra;
-    } else if (len1 < 0) {
-      sum[j] = DIGIT(num2[len2-1]) + extra;
-    } else if ( len2 < 0) {
-      sum[j] = DIGIT(num1[len1-1]) + extra;
+    sum[j] = 0;
+    if (len1 > 0) {
+      sum[j] += DIGIT(num1[len1-1]);
     }
+    if ( len2 > 0) {
+      sum[j] += DIGIT(num2[len2-1]);
+    }
+    sum[j] += extra;
+
     if (sum[j] > 9) {extra = 1; sum[j] -= 10;}
     else extra = 0;
     printf("extra is %d, %d\n", extra, sum[j]);
@@ -43,7 +47,6 @@ void add() {
   if(extra == 1) sum[j] = 1;
   else sum[j] = 0;
 
-  printf("%d, %d %d \n", len1, len2, j);
   printf("add result is ");
   for (int i = 0; i < slen ; i++) printf("%d", sum[i]);
   printf("\n");
@@ -54,65 +57,33 @@ int main() {
   int N, T;
   freopen("input.txt", "r", stdin);
   scanf("%d", &T);
-  printf("T is %d\n", T);  
+
   for(int t = 0; t < T; t++) {
     scanf("%d", &N);
-    printf("N is %d\n", N);
+
     for(int i = 0; i < N; i++) {
        int ch = -1, idx = 0;
        printf("skip: %d \n", getchar());
 
        while((ch = getchar()) != '\n') {
-         printf("[%d] %d ", idx, ch - '0');
+//         printf("[%d] %d ", idx, ch - '0');
          num1[idx] = ch;
          idx++;
        }
        num1[idx] = '\0';
-       printf("idx: %d\n", idx);
+
        ch = -1, idx = 0;
        while((ch = getchar()) != '\n') {
          num2[idx] = ch;
          idx++;
        }
-       num1[idx] = '\0';
+       num2[idx] = '\0';
 
        add();
     }
 
-    
+
   }
 
   return 0;
 }
-/*
-int main() {
-
-  while(1) {
-    printf("1. For Input Integer\n2. Add\n3. Exit\n");
-    int cmd = 0;
-    scanf("%d", &cmd);
-    switch(cmd) {
-      case 2:
-      {
-	add();
-        continue;
-      }
-      case 1:
-      {
-        int ch = getch();
-        while(ch != EOF) {
-          putchar(ch); 
-          ch = getch();
-        }
-        printf("Enter pressed \n");
-        continue;
-      }
-      case 3:
-      default:
-	break;
-    }
-  }
-  printf("The End\n");
-  return 0;
-}
-*/
